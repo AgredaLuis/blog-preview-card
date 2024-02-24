2852c954811e7c10c1e46df0780e2a0fb520c73e
AgredaLuis <Luis.Agreda98@gmail.com>
2024-02-24 00:43:42 -0400

    refact font and margin

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-Yellow p-4">
       <div className="bg-White p-4 rounded-2xl border border-black flex flex-col gap-3 max-w-[365px] duration-500 ease-out my-custom-shadow">
-        <img src={illustration} alt="svg" className="rounded-lg pb-4" />
+        <img src={illustration} alt="svg" className="mb-3 rounded-lg" />
         {/* tags */}
         <div className="text-black bg-Yellow rounded px-4 py-1 text-base w-fit font-extrabold">
           Leaning
